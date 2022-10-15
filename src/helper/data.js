040e3ce63650134b9064cae49bb8ad634b91951e
harunkesmegulu <kesme1213@gmail.com>
2022-10-16 02:06:08 +0300

    start

---
+++ src/helper/data.js
@@ -0,0 +1,51 @@
+import css from "../assets/css.png";
+import python from "../assets/python.png";
+import java from "../assets/java.png";
+import cpp from "../assets/cpp.png";
+import go from "../assets/go.png";
+import php from "../assets/php.png";
+import linkedinImg from img ="src:https://imgs.search.brave.com/d8a7MRCvZNlpRHYL3sxFQd6ZLz6u5BjUjJwZoKLEdcA/rs:fit:750:750:1/g:ce/aHR0cHM6Ly80LmJw/LmJsb2dzcG90LmNv/bS8tWk1GcGEybzc1/VHMvVklCd1BpdXAw/MEkvQUFBQUFBQUFO/T1EvcUhwNHJ3T3VP/czAvczE2MDAvbG9y/ZC5wbmc"
+import html from "../assets/html.png";
+
+export const data = [
+  {
+    name: "Harun",
+    img: linkedinImg,
+    options: ["Website Development", "1995", "Brendan Eich"],
+  },
+  {
+    name: "HTML",
+    img: html,
+    options: ["Website Development", "1993", "Tim Berners-Lee"],
+  },
+  {
+    name: "CSS",
+    img: css,
+    options: ["Website Design", "1994", "Håkon Wium Lie"],
+  },
+  {
+    name: "Python",
+    img: python,
+    options: ["AI", "1980", "Guido van Rossum"],
+  },
+  {
+    name: "Java",
+    img: java,
+    options: ["Backend", "1990s", "Sun Microsystems"],
+  },
+  {
+    name: "C++",
+    img: cpp,
+    options: ["Game Dev.", "1980s", "Bjarne Stroustrup"],
+  },
+  {
+    name: "Go",
+    img: go,
+    options: ["Network", "2009", "Robert Griesemer"],
+  },
+  {
+    name: "PHP",
+    img: php,
+    options: ["Backend", "1995", "	Rasmus Lerdorf"],
+  },
+];
