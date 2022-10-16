115fc77802e9e39df27ee7706c1770922071260c
harunkesmegulu <kesme1213@gmail.com>
2022-10-17 00:38:15 +0300

    something-wrong

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
 import Member from "./Members"
+import Container  from "react-bootstrap/Container";
+import {data} from "../helper/data"
 
 const Card = () => {
+  console.log(data);
   return (
-    <div>
-      <h1>Card</h1>
-      <Member/>
+    <Container className="Contain"style={{background:"black"}}>
+      <h1>Partners</h1>
+      {data.map((member)=>{
+        return(  <Member={...member}/>
+
+        )
+      })}
+      
 
       
-    </div>
+    </Container>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import Container from "react-bootstrap/Container";
 import Image from 'react-bootstrap/Image';
-import codeAnkaLogo from "../assets/anka.jpg";
+// import codeAnkaLogo from "../assets/anka.jpg";
 
 const Header = () => {
   return (
     <Container>
-      <Image fluid src={codeAnkaLogo} width="250px"> </Image>
+      {/* <Image fluid src={codeAnkaLogo} width="250px"> </Image> */}
     </Container>
   )
 };
